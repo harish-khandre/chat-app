48467e7cc02ac8e95cb9882f39901d6a7d602c45
Harish <harishkhandre2901@gmail.com>
2023-10-19 14:20:05 +0530

    final changes

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -7,7 +7,7 @@ import { getServerSession } from 'next-auth'
 import Image from 'next/image'
 import { notFound } from 'next/navigation'
 
-// The following generateMetadata functiion was written after the video and is purely optional
+// The following generateMetadata function was written after the video and is purely optional
 export async function generateMetadata({
   params,
 }: {
